36d34688ca772b86c27b8ec6303e7fa2d0b06de1
0xKubitus <n.cubilier@gmail.com>
2022-08-04 22:27:55 +0200

    adding localStorage feature

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import uuid from "react-uuid";
 import Sidebar from './Sidebar';
 import Main from './Main';
@@ -7,9 +7,13 @@ import './App.css';
 
 
 function App() {
-  const [notes, setNotes] = useState([]);
+  const [notes, setNotes] = useState(JSON.parse(localStorage.notes) || []);
   const [activeNote, setActiveNote] = useState(false);
 
+
+  useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes)); 
+  }, [notes]);
 
   const onAddNote = () => {
     const newNote = {
