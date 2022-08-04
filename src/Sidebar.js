dadcb43449bedc059300ee335dfa095a94b25a07
0xKubitus <n.cubilier@gmail.com>
2022-08-04 22:36:32 +0200

    now try to modify the localStorage feature so that it's being triggered by a 'Save' button

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -7,7 +7,7 @@ export default function Sidebar({ notes, onAddNote, onDeleteNote, activeNote, se
 
       <div className="app-sidebar-header">
         <h1>0xKubitus Notes</h1>
-        <button onClick={onAddNote}>Add</button>
+        <button onClick={onAddNote}>New Note</button>
       </div>
 
       <div className="app-sidebar-notes">
@@ -26,6 +26,7 @@ export default function Sidebar({ notes, onAddNote, onDeleteNote, activeNote, se
               Last modified: {new Date (note.lastModified).toLocaleDateString("fr-FR", {
                 hour: "2-digit",
                 minute: "2-digit",
+                second: "2-digit",
               })}
             </small>
           
