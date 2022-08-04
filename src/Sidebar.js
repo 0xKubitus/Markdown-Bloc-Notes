b7e728fa05232b44ce5f8edc560034761b753ca8
0xKubitus <n.cubilier@gmail.com>
2022-08-04 15:43:44 +0200

    basic rendering of the sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,3 +1,27 @@
 export default function Sidebar() {
-  return <p>Sidebar</p>
-}
+  return (
+    <div className="app-sidebar">
+
+      <div className="app-sidebar-header">
+        <h1>0xKubitus Notes</h1>
+        <button>Add</button>
+      </div>
+
+      <div className="app-sidebar-notes">
+        <div className="app-sidebar-note">
+
+          <div className="sidebar-note-title">
+            <strong>TITLE</strong>
+            <button>Delete</button>
+          </div>
+
+          <p>Note preview</p>
+
+          <small className="note-meta">Last modified [date]</small>
+        
+        </div>
+      </div>
+
+    </div>
+  );
+};
