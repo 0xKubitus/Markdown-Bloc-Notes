3ddd17f17fc463576f02070919dc01083223ce2b
0xKubitus <n.cubilier@gmail.com>
2022-11-14 18:25:51 +0100

    input states are updated when user writes down in MarkdownInput component's field

--- src/components/MarkdownInput/MarkdownInput.jsx
+++ src/components/MarkdownInput/MarkdownInput.jsx
@@ -1,25 +1,40 @@
+import { useState } from 'react';
 import TextareaAutosize from 'react-textarea-autosize';
 
 import './styles.css'
 
-export default function MarkdownInput() {
+const MarkdownInput = () => {
+  const [inputTitle, setInputTitle] = useState('');
+  const [mkdInput, setMkdInput] = useState('');
+
+  const updateInputTitle = (event) => {
+    setInputTitle(event.target.value);
+    console.log(inputTitle);
+  };
+
+  const updateMkdInput = (event) => {
+    setMkdInput(event.target.value);
+    console.log(mkdInput);
+  };
 
 
   return (
       <div className="markdown-input-container">
 
           <div>
-              <input type="text" id="markdown-input-title" className="markdown-input" placeholder="new Note's title (no Markdown here)" autoFocus />
+              <input type="text" id="markdown-input-title" className="markdown-input" placeholder="new Note's title (no Markdown here)" onChange={updateInputTitle} />
           </div>
 
           <div>
-              <TextareaAutosize id="markdown-input-content" className="markdown-input"  placeholder="Your Markdown Note's content..." />
+              <TextareaAutosize id="markdown-input-content" className="markdown-input" placeholder="Your Markdown Note's content..." onChange={updateMkdInput} autoFocus />
           </div>
 
-          <button>Save Note</button>
+          <button id="save-note-btn">Save Note</button>
 
         
       </div>
   );
 
 };
+
+export default MarkdownInput;
